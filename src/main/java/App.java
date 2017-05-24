e4a2cfd9e3d43684a288b0975fce5aff1ab0c83e
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-05-24 18:47:47 -0300

    Adding user interface command line

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,16 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-
     System.out.println("I'm a ping-pong application!");
 
+    Console myConsole = System.console();
+    System.out.println("Enter a number:");
+    int intUserNumber = Integer.parseInt(myConsole.readLine());
+
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+    System.out.println(pingPongResult);
   }
 }
