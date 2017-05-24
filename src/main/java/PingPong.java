afdc568a371dff47038944e8a4d38dd270d16cfe
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-05-24 16:37:05 -0300

    Initial files

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class PingPong {
+  public List<Object> runPingPong(int countUpTo) {
+    List<Object> result = new ArrayList<Object>();
+    for (int i = 1; i <= countUpTo; i++) {
+        if (i % 3 == 0) {
+            result.add("ping");
+        } else {
+            result.add(i);
+        }
+    }
+    return result;
+  }
+}
